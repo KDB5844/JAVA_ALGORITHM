9ceceb6702ca9306757005b6ebb007bfdf9a2cd2
Subin Cho <sbjo7713@gmail.com>
2022-05-15 00:52:56 +0900

    문자 뒤집기(reverse(), 직접 구현)

---
+++ src/main/java/_01_string/ReverseWord.java
@@ -0,0 +1,41 @@
+package main.java._01_string;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성
+ *
+ * 입력
+ * 첫 줄에 자연수 N(3<=N<=20)이 주어집니다.
+ * 두 번째 줄부터 N개의 단어가 각 줄에 하나씩 주어집니다. 단어는 영어 알파벳으로만 구성되어 있습니다.
+ *
+ * 출력
+ * N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력합니다.
+ */
+public class ReverseWord {
+
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ReverseWord T = new ReverseWord();
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        String[] inputArray = new String[cnt];
+        for (int i = 0; i < cnt; i++) {
+            inputArray[i] = sc.next();
+        }
+        for (String x : T.solution(cnt, inputArray)) {
+            System.out.println(x);
+        }
+
+    }
+
+}
