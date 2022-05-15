edfb8f21e8f76f857bd9a3e6035ec55718259e02
Subin Cho <sbjo7713@gmail.com>
2022-05-16 01:15:53 +0900

    문자 뒤집기(특수문자는 제외)

---
+++ src/main/java/_01_string/ReverseWordSpecific.java
@@ -0,0 +1,41 @@
+package main.java._01_string;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReverseWordSpecific {
+
+    public String solution(String arr) {
+        ArrayList<String> answer  = new ArrayList<>();
+        char[] c = arr.toCharArray();
+        int lt = 0, rt = c.length - 1;
+
+        while (lt < rt) {
+            if (!isAlphabet(c[lt])) lt ++;
+            else if (!isAlphabet(c[rt])) rt--;
+            else {
+                char tmp = c[lt];
+                c[lt] = c[rt];
+                c[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+
+        return String.valueOf(c);
+    }
+
+    public boolean isAlphabet(char c) {
+        if (64 <  c && c <= 122) return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ReverseWordSpecific T = new ReverseWordSpecific();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        System.out.println(T.solution(input));
+    }
+
+}
